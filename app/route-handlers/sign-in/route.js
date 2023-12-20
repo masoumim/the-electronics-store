a01d646af56e1aeb7bf645c48352d1bfecd0cdf5
Mark Masoumi <masoumi.mark@outlook.com>
2023-12-20 18:36:27 -0500

    Trying /sign-in route handler that doesn't post idToken to backend and only signs in the user

--- app/route-handlers/sign-in/route.js
+++ app/route-handlers/sign-in/route.js
@@ -5,18 +5,35 @@ import { getFirebaseAuth } from '@/app/firebase/config';
 import { sendIdToken } from '@/app/api/api';
 const auth = getFirebaseAuth();
 
+// export async function POST(request) {
+//     const req = await request.json();
+//     console.log(req);
+//     // Sign into Firebase Auth
+//     const result = signInWithEmailAndPassword(auth, req.email, req.password)
+//         .then(response => response)
+//         .then(userCredentials => {
+//             // Send the user's idToken to the Express.js backend API for server-side authentication
+//             sendIdToken(userCredentials._tokenResponse.idToken);
+//         })
+//         .catch((error) => {
+//             throw error;
+//         })    
+//     return Response.json(result);
+// }
+
+
 export async function POST(request) {
     const req = await request.json();
-    console.log(req);
-    // Sign into Firebase Auth
-    const result = signInWithEmailAndPassword(auth, req.email, req.password)
-        .then(response => response)
-        .then(userCredentials => {
-            // Send the user's idToken to the Express.js backend API for server-side authentication
-            sendIdToken(userCredentials._tokenResponse.idToken);
+    signInWithEmailAndPassword(auth, req.email, req.password)
+        .then((userCredential) => {
+            // Signed in 
+            const user = userCredential.user;
+            // ...
         })
         .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
             throw error;
-        })    
-    return Response.json(result);
+        });
+    return Response.json("User signed in");
 }
