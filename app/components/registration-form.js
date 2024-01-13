dda5cb76b6563a11a88222df8749f0f628e4c3f7
Mark Masoumi <masoumi.mark@outlook.com>
2024-01-13 17:16:03 -0500

    added client-side validation for registration form which matches the backend validation rules

--- app/components/registration-form.js
+++ app/components/registration-form.js
@@ -97,14 +97,15 @@ export default function RegistrationForm() {
                         <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-first-name">
                             First Name
                         </label>
-                        <input onChange={handleInput} value={inputFirstName} name="firstName" className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="" />
-                        <p className="text-red-500 text-xs italic">Please fill out this field.</p>
+                        <input onChange={handleInput} required minLength={1} maxLength={50} pattern="^[A-Za-z]{1,50}$" value={inputFirstName} name="firstName" id="grid-first-name" type="text" placeholder="" className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"/>
+                        <p className="text-gray-600 text-xs italic">Letters only, 50 character max</p>                     
                     </div>
                     <div className="w-full md:w-1/2 px-3">
                         <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-last-name">
                             Last Name
                         </label>
-                        <input onChange={handleInput} value={inputLastName} name="lastName" className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="" />
+                        <input onChange={handleInput} required minLength={1} maxLength={50} pattern="^[A-Za-z]{1,50}$" value={inputLastName} name="lastName" id="grid-last-name" type="text" placeholder="" className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" />
+                        <p className="text-gray-600 text-xs italic">Letters only, 50 character max</p>
                     </div>
                 </div>
                 <div className="flex flex-wrap -mx-3 mb-6">
@@ -112,8 +113,9 @@ export default function RegistrationForm() {
                         <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-email">
                             Email Address
                         </label>
-                        <input onChange={handleInput} value={inputEmail} name="email" className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-email" type="email" placeholder="" />
-                        <p className="text-gray-600 text-xs italic">Make it as long and as crazy as you'd like</p>
+                        {/* RegEx pattern from: https://www.w3schools.com/tags/tryit.asp?filename=tryhtml5_input_pattern4 */}
+                        <input onChange={handleInput} required pattern="[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,}$" value={inputEmail} name="email" className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-email" type="email" placeholder="" />
+                        <p className="text-gray-600 text-xs italic">Your email address will be how we identify you</p>
                     </div>
                 </div>
                 <div className="flex flex-wrap -mx-3 mb-6">
@@ -121,8 +123,8 @@ export default function RegistrationForm() {
                         <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-password">
                             Password
                         </label>
-                        <input onChange={handleInput} value={inputPassword} name="password" className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-password" type="password" placeholder="******************" />
-                        <p className="text-gray-600 text-xs italic">Make it as long and as crazy as you'd like</p>
+                        <input onChange={handleInput} required value={inputPassword} name="password" className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-password" type="password" placeholder="******************" />
+                        <p className="text-gray-600 text-xs italic"></p>
                     </div>
                 </div>
                 <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
