c882862995d3227ea16257777d17afe4dc0d0a52
Mark Masoumi <masoumi.mark@outlook.com>
2024-04-23 20:53:06 -0400

    Refactor CarouselComponent to use Splide library for image carousel

--- app/components/carousel.js
+++ app/components/carousel.js
@@ -1,4 +1,5 @@
 // This component is a carousel that will be displayed on the home page.
+// The carousel uses the Splide library to create a simple image carousel.(https://splidejs.com/integration/react-splide/)
 'use client';
 
 import React from 'react';
@@ -7,6 +8,7 @@ import '@splidejs/react-splide/css';
 
 export default function CarouselComponent() {
     return (
+        // Note: The div container must be the same width as the images in order for the carousel to display correctly
         <div className="flex justify-center" style={{ maxWidth: '1200px', margin: '0 auto' }}>
             <Splide options={{
                 type: 'loop',
